6dd284682abf2b51e31269738f609b33977199b0
Sheikh-Abdul-Saboor <168679145+Sheikh-Abdul-Saboor@users.noreply.github.com>
2025-09-11 23:16:33 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ function appendToDisplay(input) {
 function clearDisplay() {
   display.value = "";
 }
-
+function clearLastNumber() {
+  display.value = display.value.slice(0, -1);
+}
 function calculate() {
   try {
     display.value = eval(display.value);
@@ -40,3 +42,4 @@ document.addEventListener("keydown", (event) => {
     clearDisplay();
   }
 });
+
